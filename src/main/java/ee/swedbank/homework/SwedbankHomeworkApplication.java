81318521dae34fa5d723ee9b3a3100e982703f5d
Janar Rahumeel <janar.rahumeel@gmail.com>
2024-03-19 12:25:01 +0200

    [SWEDBANK-1] Create initial application

---
+++ src/main/java/ee/swedbank/homework/SwedbankHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ee.swedbank.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwedbankHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwedbankHomeworkApplication.class);
+    }
+
+}
